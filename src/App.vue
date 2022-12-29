e24c8bd3c7b7742190accc79e688ad275416ca6c
imStav <saprocontact@gmail.com>
2022-12-29 10:43:16 +0100

    New light and dark themes
    & Changed the accent color.

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ const errorMessage = ref("")
 const notes = ref([])
 
 const addNote = () => {
-  if (newNote.value.length < 1) {
+  if (newNote.value.length < 3) {
     return errorMessage.value = "A note needs to have at least 10 characters."
   }
 
@@ -19,6 +19,7 @@ const addNote = () => {
 
   showModal.value = false
   newNote.value = ""
+  errorMessage.value = ""
 }
 
 const deleteNote = (note) => {
@@ -35,23 +36,23 @@ const deleteNote = (note) => {
       </div>
 
       <div class="flex flex-wrap gap-6 my-14">
-        <div v-for="note in notes" :key="note.id" class="flex flex-col justify-between w-60 h-60 p-4 bg-slate-200 text-slate-700 rounded">
+        <div v-for="note in notes" :key="note.id" class="flex flex-col justify-between w-60 h-60 p-4 bg-slate-100 text-slate-700 dark:bg-sky-900 dark:text-slate-400 rounded">
           <div>
             <p class="text-sm italic opacity-50 mb-2">{{ note.date.toLocaleDateString("fr-FR") }}</p>
             <p>#{{ note.id }}</p>
             <p class="main-text">{{ note.text }}</p>
           </div>
-          <button @click="deleteNote(note)" class="flex self-end text-red-700 bg-red-300 w-fit px-2 rounded-full hover:saturate-150 duration-150">delete 🗑️</button>
+          <button @click="deleteNote(note)" class="flex self-end text-red-100 bg-red-500 w-fit px-2 rounded-full hover:saturate-150 duration-150">delete</button>
         </div>
       </div>
     </div>
 
-    <dialog v-if="showModal" class="flex p-0 rounded">
-      <div class="flex flex-col justify-center items-center rounded shadow-2xl">
-        <textarea v-model.trim="newNote" name="note" id="note" class="font-medium w-80 h-80 p-4 outline-none"></textarea>
+    <div v-if="showModal" class="fixed bottom-0 p-0 w-full border-t-[1px] border-sky-300 backdrop-blur-md">
+      <div class="flex flex-col justify-center items-center">
+        <textarea v-model.trim="newNote" name="note" id="note" class="font-medium w-80 h-80 p-4 outline-none bg-transparent"></textarea>
         <p v-if="errorMessage" class="text-red-500 mt-2">{{ errorMessage }}</p>
-        <button @click="addNote" class="my-4 mx-auto py-1 px-5 w-fit bg-gradient-to-br from-teal-500 to-fuchsia-400 text-slate-100 rounded-full hover:saturate-150 duration-150">Create note</button>
+        <button @click="addNote" class="my-4 mx-auto py-1 px-5 w-fit bg-sky-500 text-slate-100 rounded-full hover:saturate-150 duration-150">Create note</button>
       </div>
-    </dialog>
+    </div>
   </main>
 </template>
