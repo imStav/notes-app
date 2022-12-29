dd0b924ca097cff691d46420dfcfc92ec5128415
imStav <saprocontact@gmail.com>
2022-12-29 10:11:06 +0100

    New feature: `deleteNote`
    Now you can delete a note, with a dedicated button.
    Still working on themes.

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ const errorMessage = ref("")
 const notes = ref([])
 
 const addNote = () => {
-  if (newNote.value.length < 10) {
+  if (newNote.value.length < 1) {
     return errorMessage.value = "A note needs to have at least 10 characters."
   }
 
@@ -15,139 +15,43 @@ const addNote = () => {
     id: Math.floor(Math.random() * 100000),
     text: newNote.value,
     date: new Date(),
-    backgroundColor: "black"
   })
 
   showModal.value = false
   newNote.value = ""
 }
+
+const deleteNote = (note) => {
+  notes.value = notes.value.filter(n => n !== note)
+}
 </script>
 
 <template>
-  <main>
-    <div v-if="showModal" class="overlay">
-      <div class="modal">
-        <textarea v-model.trim="newNote" name="note" id="note" cols="30" rows="10"></textarea>
-        <p class="error" v-if="errorMessage">{{ errorMessage }}</p>
-        <button @click="addNote">Create note</button>
+  <main class="flex justify-center items-center">
+    <div class="w-4/5 xl:w-2/4 mx-auto">
+      <div class="flex justify-between items-center my-4">
+        <h1 class="text-3xl font-bold">Notes</h1>
+        <button @click="showModal = true" class=" text-3xl focus:scale-90 hover:rotate-[360deg] duration-300">🖋️</button>
       </div>
-    </div>
 
-    <div class="container">
-      <header>
-        <h1>Notes</h1>
-        <button @click="showModal = true">+</button>
-      </header>
-
-      <div class="cards-container">
-        <div v-for="note in notes" :key="note.id" class="card">
-          <p class="main-text">{{ note.text }}</p>
-          <p class="date">{{ note.date.toLocaleDateString("fr-FR") }}</p>
+      <div class="flex flex-wrap gap-6 my-14">
+        <div v-for="note in notes" :key="note.id" class="flex flex-col justify-between w-60 h-60 p-4 bg-slate-200 text-slate-700 rounded">
+          <div>
+            <p class="text-sm italic opacity-50 mb-2">{{ note.date.toLocaleDateString("fr-FR") }}</p>
+            <p>#{{ note.id }}</p>
+            <p class="main-text">{{ note.text }}</p>
+          </div>
+          <button @click="deleteNote(note)" class="flex self-end text-red-700 bg-red-300 w-fit px-2 rounded-full hover:saturate-150 duration-150">delete 🗑️</button>
         </div>
       </div>
     </div>
+
+    <dialog v-if="showModal" class="flex p-0 rounded">
+      <div class="flex flex-col justify-center items-center rounded shadow-2xl">
+        <textarea v-model.trim="newNote" name="note" id="note" class="font-medium w-80 h-80 p-4 outline-none"></textarea>
+        <p v-if="errorMessage" class="text-red-500 mt-2">{{ errorMessage }}</p>
+        <button @click="addNote" class="my-4 mx-auto py-1 px-5 w-fit bg-gradient-to-br from-teal-500 to-fuchsia-400 text-slate-100 rounded-full hover:saturate-150 duration-150">Create note</button>
+      </div>
+    </dialog>
   </main>
 </template>
-
-<style scoped>
-main {
-  height: 100vh;
-  width: 100vw;
-}
-
-.container {
-  max-width: 1000px;
-  padding: 10px;
-  margin: 0 auto;
-}
-
-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-block: 2rem;
-}
-
-h1 {
-  font-weight: bold;
-  font-size: 2.5rem;
-}
-
-header button {
-  border: none;
-  height: 2.5rem;
-  width: 2.5rem;
-  border-radius: 100%;
-  background-color: black;
-  color: whitesmoke;
-  font-size: 1.2rem;
-  font-weight: bold;
-  cursor: pointer;
-}
-
-textarea {
-  padding: 1rem 1.2rem;
-}
-
-.cards-container {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 1rem;
-}
-
-.card {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  width: 225px;
-  height: 225px;
-  background-color: black;
-  color: whitesmoke;
-  border-radius: 10px;
-  padding: 1rem 1.5rem;
-}
-
-.date {
-  font-weight: bold;
-}
-
-.overlay {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background-color: #00000046;
-  z-index: 10;
-}
-
-.modal {
-  display: flex;
-  flex-direction: column;
-  padding: 2rem;
-  position: relative;
-  width: 750px;
-  background-color: whitesmoke;
-  color: black;
-  border-radius: 10px;
-}
-
-.modal button {
-  padding: 0.5rem 1.5rem;
-  background-color: black;
-  color: whitesmoke;
-  border: none;
-  margin-top: 1rem;
-  border-radius: 10px;
-  cursor: pointer;
-}
-
-.modal .close {
-  background-color: #de3131;
-}
-
-.error {
-  color: #de3131;
-}
-</style>
