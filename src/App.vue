4a2a1c705c08cea238716a96b2a7782615b6451c
imStav <saprocontact@gmail.com>
2022-12-29 10:50:06 +0100

    New feature: handling the `@keypress.enter`
    User can now both use the click (button 'Create note') or the enter key (@keypress.enter) to create a note.

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ const notes = ref([])
 
 const addNote = () => {
   if (newNote.value.length < 3) {
-    return errorMessage.value = "A note needs to have at least 10 characters."
+    return errorMessage.value = "A note needs to have at least 3 characters."
   }
 
   notes.value.push({
@@ -39,17 +39,20 @@ const deleteNote = (note) => {
         <div v-for="note in notes" :key="note.id" class="flex flex-col justify-between w-60 h-60 p-4 bg-slate-100 text-slate-700 dark:bg-sky-900 dark:text-slate-400 rounded">
           <div>
             <p class="text-sm italic opacity-50 mb-2">{{ note.date.toLocaleDateString("fr-FR") }}</p>
-            <p>#{{ note.id }}</p>
             <p class="main-text">{{ note.text }}</p>
           </div>
-          <button @click="deleteNote(note)" class="flex self-end text-red-100 bg-red-500 w-fit px-2 rounded-full hover:saturate-150 duration-150">delete</button>
+
+          <div class="flex justify-between">
+            <p class="opacity-50">#{{ note.id }}</p>
+            <button @click="deleteNote(note)" class="flex self-end text-red-100 bg-red-500 w-fit px-2 rounded-full hover:saturate-150 duration-150">delete</button>
+          </div>
         </div>
       </div>
     </div>
 
     <div v-if="showModal" class="fixed bottom-0 p-0 w-full border-t-[1px] border-sky-300 backdrop-blur-md">
       <div class="flex flex-col justify-center items-center">
-        <textarea v-model.trim="newNote" name="note" id="note" class="font-medium w-80 h-80 p-4 outline-none bg-transparent"></textarea>
+        <textarea v-model.trim="newNote" @keypress.enter="addNote" name="note" id="note" class="font-medium w-80 h-80 p-4 outline-none bg-transparent"></textarea>
         <p v-if="errorMessage" class="text-red-500 mt-2">{{ errorMessage }}</p>
         <button @click="addNote" class="my-4 mx-auto py-1 px-5 w-fit bg-sky-500 text-slate-100 rounded-full hover:saturate-150 duration-150">Create note</button>
       </div>
